e582dc9edebacb079242805d4f16ff441e903875
Andrés Contreras Guillén <andres.contreras@dworkers.ai>
2025-04-21 13:25:08 +0200

    Refactor imports to use updated FilterableId annotation
    Replaced `com.catalis.common.core.filters.FilterableId` with `com.catalis.core.utils.annotations.FilterableId` across multiple DTO classes to reflect the updated package structure. This ensures consistency and proper usage of the new annotation location.

--- core-lending-factoring-interfaces/src/main/java/com/catalis/core/lending/factoring/interfaces/dtos/invoice/v1/FactoringInvoiceStatusHistoryDTO.java
+++ core-lending-factoring-interfaces/src/main/java/com/catalis/core/lending/factoring/interfaces/dtos/invoice/v1/FactoringInvoiceStatusHistoryDTO.java
@@ -1,7 +1,7 @@
 package com.catalis.core.lending.factoring.interfaces.dtos.invoice.v1;
 
-import com.catalis.common.core.filters.FilterableId;
 import com.catalis.core.lending.factoring.interfaces.enums.invoice.v1.InvoiceStatusEnum;
+import com.catalis.core.utils.annotations.FilterableId;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
